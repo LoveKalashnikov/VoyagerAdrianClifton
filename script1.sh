f8b5a8ceb7c49b05112fa899c26ef0a2827fda2f
Cliftech <adrian_clifton222@yahoo.com>
2016-05-24 07:49:38 +0800

    memory top 10 process print

--- script1.sh
+++ script1.sh
@@ -1,36 +1,56 @@
  #!/bin/bash
 TOTAL_MEMORY=$( free | grep Mem: | awk '{print $2 }' )
 USED_MEMORY=$( free | grep Mem: | awk '{print $3 }' )
-
+TOP_PROCESSES=$( ps aux | sort -nk +4 | tail | awk '{print $11}')
 while getopts c:w:e: j; do
-  echo $j
   if [ $j = "c" ]; then
   	c=$OPTARG
-  	echo $c
-  	echo "got in 1"
   elif [ $j = "w" ]; then
   	w=$OPTARG
-  	echo $w
-  	echo $c
-  	echo "got in 2"
   elif [ $j = "e" ]; then
   	e=$OPTARG
-  	echo $e
-  	echo "got in 3"
   fi
 done
-echo $USED_MEMORY
 
+echo $TOP_PROCESSES
+if [ "$c" = "" ]; then 
+	echo "put value for critical threshold (c)"
+	exit
+fi
+if [ "$w" = "" ]; then
+	echo "put value for warning threshold (w)"
+	exit
+fi
+if [ "$e" = "" ]; then
+	echo "put value for email (e)"
+	exit
+fi
 criticalMem=$((( $TOTAL_MEMORY / 100 ) * $c ))
 warningMem=$((( $TOTAL_MEMORY / 100 ) * $w ))
 
-echo $criticalMem
-echo $warningMem
+if [ "$w" -gt 100 ]; then
+	echo "w must be less than 100"
+	exit
+fi
+
+if [ "$c" -gt 100 ]; then
+	echo "c must be less than 100"
+	exit
+fi
+
+if [ "$w" -gt "$c" ]; then
+	echo "w must be less than c"
+	exit
+fi
+
 if [ "$USED_MEMORY" -ge "$criticalMem" ]; then
-	echo 'a'
+	echo 2
+	exit 2
 elif [ "$USED_MEMORY" -ge "$warningMem" ]; then
-	echo 'b'
+	echo 1
+	exit 1
 else
-	echo 'c'
+	echo 0
+	exit 0
 fi
 
