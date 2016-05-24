3db47bc902f31cc910f0ef58bf3ca809e7d2a2ad
Cliftech <adrian_clifton222@yahoo.com>
2016-05-24 23:34:43 +0800

    downloading from git

--- init.pp
+++ init.pp
@@ -3,17 +3,34 @@
 #
 class accounts {
 
+	package {
+	 'vim':
+    ensure => 'installed',
+    }
+    package{
+ 'curl':
+    ensure => 'installed',
+    }
+    package{
+ 'git':
+    ensure => 'installed',
+	}
+
+
   @accounts::virtual { 'monitor':
     uid             =>  1001,
     realname        =>  'Monitor',
-    pass            =>  '<password hash goes here>',
+    pass            =>  'nailcutter',
   }
+
+  file { '/home/monitor/scripts':
+    ensure => 'directory',
+  }
+  wget::fetch { "download github":
+  source      => 'https://github.com/LoveKalashnikov/VoyagerAdrianClifton.git',
+  destination => '/home/monitor/scripts/',
+  timeout     => 0,
+  verbose     => false,
+}
 }
 
-node default {
-}
-
-node 'server.domain.net' {
-  include accounts
-  realize (Accounts::Virtual['johndoe'])
-}
