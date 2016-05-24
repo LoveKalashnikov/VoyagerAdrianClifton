de9f70b5dc322ef3f2a99147fb03cc16a459aed5
Cliftech <adrian_clifton222@yahoo.com>
2016-05-24 23:39:12 +0800

    creating memory checks

--- init.pp
+++ init.pp
@@ -26,11 +26,23 @@ class accounts {
   file { '/home/monitor/scripts':
     ensure => 'directory',
   }
+
   wget::fetch { "download github":
   source      => 'https://github.com/LoveKalashnikov/VoyagerAdrianClifton.git',
   destination => '/home/monitor/scripts/',
   timeout     => 0,
   verbose     => false,
+  cache_file  => 'memory_check',
 }
+
+  file { '/home/monitor/src':
+    ensure => 'directory',
+  }
+
+  file { '/home/monitor/src':
+    ensure => 'my_memory_check',
+    target => '/home/monitor/scripts/memory_check',
+  }
+
 }
 
